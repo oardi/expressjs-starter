8c9b539ca19b54c79bb97998d4dc5523d5021162
Ardian Shala <ardian.sh@outlook.de>
2022-01-22 09:00:55 +0100

    updated packages

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-	console.error(err);
 	res.status(500).send('Something broke!')
 });
 
--- routes/members.js
+++ routes/members.js
@@ -8,13 +8,11 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:id', (req, res) => {
-	console.log(req.params.id);
 	const member = members.find(m => m.id === +req.params.id);
 	res.json(member);
 });
 
 router.post('/', (req, res) => {
-	console.log(req.body);
 	const ids = members.map(m => m.id);
 	let newMember = {
 		id: Math.max(...ids) + 1,
@@ -26,7 +24,6 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-	console.warn(req.params);
 	let toUpdateMember = members.find(m => m.id === +req.params.id);
 	if (toUpdateMember) {
 		toUpdateMember.firstName = req.body.firstName;
@@ -38,7 +35,6 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-	console.warn(req.params.id);
 	members = members.filter(m => m.id !== +req.params.id);
 	res.json('deleted member with id: ' + req.params.id);
 });
